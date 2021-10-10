148582035d1a7ead68a95adc0a1b30284a86f72e
Gian Ortiz <brksgian@gmail.com>
2021-10-10 15:36:41 -0300

    Allow any Origin access to the API /duodigit endpoint
    This project does not intend to be used in production, we can allow
    Origin access from any source, in a production API we would select
    the origins we wish to provide responses.

--- backend/src/main/java/com/gianortiz/duodigit/DuodigitCalculusController.java
+++ backend/src/main/java/com/gianortiz/duodigit/DuodigitCalculusController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DuodigitCalculusController {
     
     @GetMapping(value="/duodigit", produces=MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "*")
     public DuodigitCalculus duodigitCalculus(
         @RequestParam("number") @Min(100) long number) {
         return new DuodigitCalculus(number);
