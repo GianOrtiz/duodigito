dbe640c38166ca19db55ce5abc98042138f6799d
Gian Ortiz <brksgian@gmail.com>
2021-10-10 11:43:57 -0300

    Init backend with Spring Boot

---
+++ backend/src/main/java/com/gianortiz/duodigit/DuodigitApplication.java
@@ -0,0 +1,13 @@
+package com.gianortiz.duodigit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuodigitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuodigitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gianortiz/duodigit/DuodigitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianortiz.duodigit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuodigitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
