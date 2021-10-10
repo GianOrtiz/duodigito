d2f43b630123a7c8d499aa56737c58e3826b8a29
Gian Ortiz <brksgian@gmail.com>
2021-10-10 14:54:02 -0300

    Add duodigit calculus controller to REST service

---
+++ backend/src/main/java/com/gianortiz/duodigit/DuodigitCalculusController.java
@@ -0,0 +1,21 @@
+package com.gianortiz.duodigit;
+
+import javax.validation.constraints.Min;
+
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Validated
+public class DuodigitCalculusController {
+    
+    @GetMapping(value="/duodigit", produces=MediaType.APPLICATION_JSON_VALUE)
+    public DuodigitCalculus duodigitCalculus(
+        @RequestParam("number") @Min(100) long number) {
+        return new DuodigitCalculus(number);
+    }
+}
