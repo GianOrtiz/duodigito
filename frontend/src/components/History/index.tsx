283b1fa05cbfb075c3fe5700900f676a358077e6
Gian Ortiz <brksgian@gmail.com>
2021-10-12 17:44:19 -0300

    Send empty message when history is empty

--- frontend/src/components/History/index.tsx
+++ frontend/src/components/History/index.tsx
@@ -9,24 +9,34 @@ export interface Props {
 const History = (props: Props): JSX.Element => {
     const { values } = props;
 
+    const renderEntries = () => {
+        return values.map<JSX.Element>((value, index) =>
+            <div
+                className="history-entry"
+                key={index.toString()}
+            >
+                <div className="input-history">{value.input}</div>
+                <div
+                    className="time-history"
+                >
+                    {value.elapsedTimeMillis}ms
+                </div>
+                <div className="result-history">{value.result}</div>
+            </div>
+        );
+    }
+
     return (
         <div className="history">
             <header className="history-header">Histórico</header>
-            {values.map<JSX.Element>(
-                (value, index) =>
-                    <div
-                        className="history-entry"
-                        key={index.toString()}
-                    >
-                        <div className="input-history">{value.input}</div>
-                        <div
-                            className="time-history"
-                        >
-                            {value.elapsedTimeMillis}ms
-                        </div>
-                        <div className="result-history">{value.result}</div>
-                    </div>
-            )}
+            {values.length > 0
+                ? renderEntries()
+                : (
+                    <span className="empty-history">
+                        Nenhum cálculo no histórico
+                    </span>
+                )
+            }
         </div>
     );
 }
